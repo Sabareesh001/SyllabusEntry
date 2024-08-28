1cfc28d9f695fe24035be3380e2c47b32f2bf6b7
Sabareesh001 <hatrickda@gmail.com>
2024-08-28 15:24:58 +0530

    working condition

--- vite-project/src/pages/syllabusEntry/SyllabusEntry.jsx
+++ vite-project/src/pages/syllabusEntry/SyllabusEntry.jsx
@@ -1,16 +1,37 @@
-import './SyllabusEntry.css'
-import Card from '../../components/card/Card'
-import Select from 'react-select'
-const SyllabusEntry = ()=>{
-    return(
-        <div className='syllabusEntryPageContainer'>
-            <Card content={
-                <div>
-                    
+import "./SyllabusEntry.css";
+import Card from "../../components/card/Card";
+import Select from "react-select";
+import { Info } from "@mui/icons-material";
+import { useState } from "react";
+import StyledModal from '../../components/modal/StyledModal'
+const SyllabusEntry = () => {
+    const [isRegulationModalOpen,setRegulationModalOpen]= useState(false)
+  return (
+    <div className="syllabusEntryPageContainer">
+      <Card
+        content={
+          <div className="selectContainer">
+            <div className="select">
+              <Select />
+              <div className="newRegulationContainer">
+              <Info/> New Regualtion?
                 </div>
-            }/>
-        </div>
-    
-    )
-}
-export default SyllabusEntry
+              
+            </div>
+            <div className="select">
+              <Select />
+            </div>
+            <div className="select">
+              <Select />
+            </div>
+            <div className="select">
+              <Select />
+            </div>
+          </div>
+        }
+      />
+      <StyledModal setOpen={setRegulationModalOpen} open={isRegulationModalOpen}/>
+    </div>
+  );
+};
+export default SyllabusEntry;
